c43e7ca8fed299d96f37654bbf785ff71c8b0973
macro <macro_fengye@163.com>
2017-05-19 10:12:42 +0800

    分离素材的依赖注入组件!

---
+++ src/Providers/MaterialTemporaryProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: macro chen <macro_fengye@163.com>
+ * Date: 17-5-19
+ * Time: 上午9:55
+ */
+
+namespace WeChat\Providers;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use WeChat\WeChat\Material\Temporary;
+
+class MaterialTemporaryProvider implements ServiceProviderInterface
+{
+    /**
+     * Registers services on the given container.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     *
+     * @param Container $pimple A container instance
+     */
+    public function register(Container $pimple)
+    {
+        $pimple['material_temporary'] = function (Container $container) {
+            $temporary = function ($pimple) {
+                return new Temporary($pimple['access_token']);
+            };
+        };
+    }
+}
